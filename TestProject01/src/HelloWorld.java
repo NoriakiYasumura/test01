8d7fcef9087628be894275da2def314dd1d11d90
NoriakiYasumura <yasumura006@gmail.com>
2019-03-13 17:32:51 +0900

    2019/03/13 update

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[] a = {"加藤","asfg","deee"};
+		a[0]="sde";
+		System.out.println(a[0]);
+
+	}
+
+}
